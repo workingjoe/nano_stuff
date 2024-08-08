d31f62410bf34f8f25613092302e8c5d55a5995a
Byron Tiller <workingjoe@gmail.com>
2024-08-08 13:45:26 -0400

    reference ROI

---
+++ openCV9_ROI.py
@@ -0,0 +1,110 @@
+import cv2
+
+print(cv2.__version__)
+# print(cv2.getBuildInformation())
+
+# create a gstreamer pipeline command for CSI cameras
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=1920,
+    display_height=1080,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=%d, height=%d, framerate=%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=%d, height=%d, format=BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
+dispW=640
+dispH=480
+flip=0
+key = 0
+# horrible string ... one long gstreamer launch
+# camSet0='nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=' + str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+# camSet1='nvarguscamerasrc sensor-id=1 ! video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=' + str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+#
+# for arducam there is a lens/camera calibration to setup...
+# https://docs.arducam.com/Nvidia-Jetson-Camera/Application-note/Fix-Red-Tint-with-ISP-Tuning/
+#
+# wget https://www.arducam.com/downloads/Jetson/Camera_overrides.tar.gz
+# tar zxvf Camera_overrides.tar.gz
+# sudo cp camera_overrides.isp /var/nvidia/nvcam/settings/
+# sudo chmod 664 /var/nvidia/nvcam/settings/camera_overrides.isp
+# sudo chown root:root /var/nvidia/nvcam/settings/camera_overrides.isp
+
+
+# camSet0 = gstreamer_pipeline( sensor_id=0, 
+#                              capture_width=3264,
+#                              capture_height=2464,
+#                              display_width=dispW,
+#                              display_height=dispH,
+#                              framerate=21,
+#                              flip_method=flip,
+#                            )
+camSet0 = gstreamer_pipeline( sensor_id=0, 
+                             capture_width=1920,
+                             capture_height=1080,
+                             display_width=dispW,
+                             display_height=dispH,
+                             framerate=30,
+                             flip_method=flip,
+                           )
+camSet1 = gstreamer_pipeline( sensor_id=1, 
+                             capture_width=1920,
+                             capture_height=1080,
+                             display_width=dispW,
+                             display_height=dispH,
+                             framerate=30,
+                             flip_method=flip,
+                           )
+print(camSet1)
+print("------------------------------------")
+
+
+# create CSI camera object
+cam = cv2.VideoCapture(camSet1)
+
+# or create USB webcam object (arg is 0,1, or 2 depends on CSI cameras installed)
+# cam = cv2.VideoCapture(2)
+
+while True:
+    ret, frame = cam.read()
+
+    roi = frame[50:250, 200:400] # roi is not a COPY, it is a REFERENCE 
+    roiGray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+    frame[50:250, 200:400]=[0,128,0] # inlay green box in ROI spot
+
+    cv2.imshow('ROI', roi)
+    cv2.moveWindow('ROI', 705, 0)
+    cv2.imshow('ROIGRAY', roiGray)
+    cv2.moveWindow('ROIGRAY', 705, 256)
+
+
+
+    cv2.imshow('theCam', frame)
+    cv2.moveWindow('theCam', 0, 0)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# clean up
+cam.release()
+cv2.destroyAllWindows()
+
