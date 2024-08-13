77c9620bf86764da56baa2e0e1d69e8c18ce9384
Byron Tiller <workingjoe@gmail.com>
2024-08-13 15:21:26 -0400

    contour added

--- openCV18_contours.py
+++ openCV18_contours.py
@@ -1,9 +1,9 @@
 import cv2
-import numpy as np 
-
-
 print(cv2.__version__)
-# print(cv2.getBuildInformation())
+import numpy as np
+ 
+def nothing(x):
+    pass
 
 # create a gstreamer pipeline command for CSI cameras
 def gstreamer_pipeline(
@@ -33,30 +33,32 @@ def gstreamer_pipeline(
         )
     )
 
-def nothingFunc(x):
-    pass
+
+
+
 
 
 cv2.namedWindow('Trackbars')
-cv2.moveWindow('Trackbars', 1320, 0)
+cv2.moveWindow('Trackbars',1320,800)
+ 
+cv2.createTrackbar('hueLower', 'Trackbars', 170,179,nothing)
+cv2.createTrackbar('hueUpper', 'Trackbars', 179,179,nothing)
+ 
+cv2.createTrackbar('hue2Lower','Trackbars',  0,179,nothing)
+cv2.createTrackbar('hue2Upper','Trackbars', 12,179,nothing)
+ 
+cv2.createTrackbar('satLow', 'Trackbars',120,255,nothing)
+cv2.createTrackbar('satHigh','Trackbars',255,255,nothing)
+cv2.createTrackbar('valLow', 'Trackbars',100,255,nothing)
+cv2.createTrackbar('valHigh','Trackbars',255,255,nothing)
+ 
+ 
+dispW = 640
+dispH = 480
+flip  = 0
 
-cv2.createTrackbar('hueLow',  'Trackbars',   4, 179, nothingFunc )
-cv2.createTrackbar('hueHigh', 'Trackbars',  54, 179, nothingFunc )
+noiseThreshold = 500
 
-cv2.createTrackbar('hue2Low',  'Trackbars', 145, 179, nothingFunc )
-cv2.createTrackbar('hue2High', 'Trackbars', 179, 179, nothingFunc )
-
-cv2.createTrackbar('satLow',  'Trackbars', 150, 255, nothingFunc )
-cv2.createTrackbar('satHigh', 'Trackbars', 255, 255, nothingFunc )
-
-cv2.createTrackbar('valLow',  'Trackbars', 150, 255, nothingFunc )
-cv2.createTrackbar('valHigh', 'Trackbars', 255, 255, nothingFunc )
-
-
-dispW=320
-dispH=240
-flip=0
-key = 0
 # horrible string ... one long gstreamer launch
 # camSet0='nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=' + str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
 # camSet1='nvarguscamerasrc sensor-id=1 ! video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=' + str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
@@ -98,79 +100,71 @@ camSet1 = gstreamer_pipeline( sensor_id=1,
 print(camSet1)
 print("------------------------------------")
 
-
-# create CSI camera object
 cam = cv2.VideoCapture(camSet1)
 
 # or create USB webcam object (arg is 0,1, or 2 depends on CSI cameras installed)
 # cam = cv2.VideoCapture(2)
 
+
 while True:
     ret, frame = cam.read()
- #   frame = cv2.imread('smarties.png')
+    #frame=cv2.imread('smarties.png')
 
-    cv2.imshow('theCam', frame)
-    cv2.moveWindow('theCam', 0, 0)
+ 
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+ 
+    hueLow=cv2.getTrackbarPos('hueLower', 'Trackbars')
+    hueUp=cv2.getTrackbarPos('hueUpper', 'Trackbars')
+ 
+    hue2Low=cv2.getTrackbarPos('hue2Lower', 'Trackbars')
+    hue2Up=cv2.getTrackbarPos('hue2Upper', 'Trackbars')
+ 
+    lowSat=cv2.getTrackbarPos('satLow', 'Trackbars')
+    upperSat=cv2.getTrackbarPos('satHigh', 'Trackbars')
+ 
+    lowVal=cv2.getTrackbarPos('valLow', 'Trackbars')
+    upperVal=cv2.getTrackbarPos('valHigh', 'Trackbars')
+ 
+    lower_Bounds=np.array([hueLow,lowSat,lowVal])
+    upper_Bounds=np.array([hueUp,upperSat,upperVal])
+ 
+    lower_Bounds2=np.array([hue2Low,lowSat,lowVal])
+    upper_Bounds2=np.array([hue2Up,upperSat,upperVal])
+ 
+    FGmask    = cv2.inRange(hsv,lower_Bounds,upper_Bounds)
+    FGmask2   = cv2.inRange(hsv,lower_Bounds2,upper_Bounds2)
+    FGmaskComp= cv2.add(FGmask,FGmask2)
 
-    # create image in HSV color space
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    hueLow = cv2.getTrackbarPos('hueLow',  'Trackbars')
-    hueUp  = cv2.getTrackbarPos('hueHigh', 'Trackbars')
-
-    # additional trackbar for RED hue since it is split around 179 - 0
-    hue2Low = cv2.getTrackbarPos('hue2Low',  'Trackbars')
-    hue2Up  = cv2.getTrackbarPos('hue2High', 'Trackbars')
-
-    satLow = cv2.getTrackbarPos('satLow',  'Trackbars')
-    satUp  = cv2.getTrackbarPos('satHigh', 'Trackbars')
-
-    valLow = cv2.getTrackbarPos('valLow',  'Trackbars')
-    valUp  = cv2.getTrackbarPos('valHigh', 'Trackbars')
-
-    lowerBound = np.array([hueLow, satLow, valLow])
-    upperBound = np.array([hueUp, satUp, valUp])
-
-    # additinal set of parms for RED hue
-    lowerBound2 = np.array([hue2Low, satLow, valLow])
-    upperBound2 = np.array([hue2Up, satUp, valUp])
-
-
-    # create Forground mask using bounds
-    FGmask  = cv2.inRange(hsv, lowerBound, upperBound)
-    # additional mask for second set of 
-    FGmask2= cv2.inRange(hsv, lowerBound2, upperBound2)
-
-    FGmaskcomposite = cv2.add(FGmask, FGmask2)
-    cv2.imshow('FGmaskcomposite', FGmaskcomposite)
-    cv2.moveWindow('FGmaskcomposite', 900,410)
+    # find contours
+    contours, dummy_hierarchy = cv2.findContours(FGmaskComp, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
-    cv2.imshow('FGmask', FGmask)
-    cv2.moveWindow('FGmask', 0,410)
+    # -1 is plot all contours
+    # cv2.drawContours(frame, contours, -1, (200,200,0),2)
+    
+    # lets sort out for the biggest, using area lambda function, reverse means BIG to SMALL
+    contours = sorted(contours, key = lambda x:cv2.contourArea(x), reverse = True)
+    
+    # step through each contour array and draw each one greater than some threshold
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area >= noiseThreshold:
+            # find bounding rectangle instead
+            (x,y,w,h) = cv2.boundingRect(cnt)
+            # cv2.drawContours(frame, [cnt], 0, (200,200,0),2)
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (200, 200, 0), 2)
 
-    # FG = cv2.bitwise_and(frame, frame, mask=FGmask)
-    FG = cv2.bitwise_and(frame, frame, mask=FGmaskcomposite)
-    cv2.imshow('FG', FG)
-    cv2.moveWindow('FG', 500,0)
+    
+    # cv2.drawContours(frame, contours, 0, (200,200,0),2)
 
-    # create BACKGROUND mask as opposite of FG
-    BGmask = cv2.bitwise_not(FGmask)
-    cv2.imshow('BGmask', BGmask)
-    cv2.moveWindow('BGmask', 480,410)
+    cv2.imshow('FGmaskComp',FGmaskComp)
+    cv2.moveWindow('FGmaskComp',0,530)
+ 
+    cv2.imshow('nanoCam',frame)
+    cv2.moveWindow('nanoCam',0,0)
 
-    # use format conversion
-    BG = cv2.cvtColor(BGmask, cv2.COLOR_GRAY2BGR)
-
-    # assemble final image
-    final = cv2.add(FG, BG)
-    cv2.imshow('Final', final)
-    cv2.moveWindow('Final', 900,0)
-
-
+ 
     if cv2.waitKey(1) == ord('q'):
         break
 
-# clean up
 cam.release()
 cv2.destroyAllWindows()
-
