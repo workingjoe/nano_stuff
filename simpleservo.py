b879000e7e61a2f894f67d2a72019ee937576f94
Byron Tiller <workingjoe@gmail.com>
2024-08-13 15:43:24 -0400

    servo testing -- use VENV from adafruit

--- simpleservo.py
+++ simpleservo.py
@@ -8,22 +8,30 @@ myKit.servo[0].angle = 90
 #[1] is pan_up_down
 myKit.servo[1].angle = 90
 
-# full sweep one direction
-# for i in range(0,180, 1):
-#     time.sleep(0.1)
-#     myKit.servo[0].angle = i
+time.sleep(1)
 
-# # full sweep back 
-# for i in range(180,0, -1):
-#     time.sleep(0.1)
-#     myKit.servo[0].angle = i
+# full sweep one direction
+for i in range(0,180, 1):
+    time.sleep(0.05)
+    myKit.servo[0].angle = i
+
+# full sweep back 
+for i in range(180,0, -1):
+    time.sleep(0.05)
+    myKit.servo[0].angle = i
 
 # # full sweep down direction
-# for i in range(0,180, 1):
-#     time.sleep(0.1)
-#     myKit.servo[1].angle = i
+for i in range(0,180, 1):
+    time.sleep(0.05)
+    myKit.servo[1].angle = i
 
 # # full sweep back up 
-# for i in range(180,0, -1):
-#     time.sleep(0.1)
-#     myKit.servo[1].angle = i
+for i in range(180,0, -1):
+    time.sleep(0.05)
+    myKit.servo[1].angle = i
+
+time.sleep(1)
+#[0] is  pan left-right
+myKit.servo[0].angle = 90
+#[1] is pan_up_down
+myKit.servo[1].angle = 90
